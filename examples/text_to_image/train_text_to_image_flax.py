25067b22a6e799380eb9f5edabd21195986d5b48
Emkay Nguyen <81498410+DiningSystem@users.noreply.github.com>
2023-04-08 19:01:50 -0500

    Update train_text_to_image_flax.py

--- examples/text_to_image/train_text_to_image_flax.py
+++ examples/text_to_image/train_text_to_image_flax.py
@@ -338,8 +338,8 @@ def main():
     )
 
     def preprocess_train(examples):
-        #images = [Image.open(io.BytesIO(image)).convert("RGB") for image in examples[image_column]]
-        images = [Image.frombytes('RGB', (args.resolution, args.resolution), image) for image in examples[image_column]]
+        images = [Image.open(io.BytesIO(image.read())).convert("RGB") for image in examples[image_column]]
+        #images = [Image.frombytes('RGB', (args.resolution, args.resolution), image) for image in examples[image_column]]
         Image.frombytes('RGB', (jpg_image_width, jpg_image_height), buffered_image)
         examples["pixel_values"] = [train_transforms(image) for image in images]
         examples["input_ids"] = tokenize_captions(examples)

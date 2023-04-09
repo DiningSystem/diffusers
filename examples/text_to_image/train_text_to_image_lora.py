4200b18e0057df2bea86a555f71f8f0b4e182bb0
Emkay Nguyen <81498410+DiningSystem@users.noreply.github.com>
2023-04-09 12:46:04 -0500

    Update train_text_to_image_lora.py

--- examples/text_to_image/train_text_to_image_lora.py
+++ examples/text_to_image/train_text_to_image_lora.py
@@ -588,7 +588,8 @@ def main():
 
     def preprocess_train(examples):
         #images = [image.convert("RGB") for image in examples[image_column]]
-        images = [Image.open(BytesIO(base64.b64decode(image))).convert("RGB") for image in examples[image_column]]
+        #images = [Image.open(BytesIO(base64.b64decode(image))).convert("RGB") for image in examples[image_column]]
+        images = [Image.open(BytesIO(image)).convert("RGB") for image in examples[image_column]]
         examples["pixel_values"] = [train_transforms(image) for image in images]
         examples["input_ids"] = tokenize_captions(examples)
         return examples

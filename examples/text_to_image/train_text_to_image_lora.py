5dc8b92bbd2b4d0004b0f993fa70be8e937c1768
Emkay Nguyen <81498410+DiningSystem@users.noreply.github.com>
2023-04-18 11:09:18 -0500

    Update train_text_to_image_lora.py

--- examples/text_to_image/train_text_to_image_lora.py
+++ examples/text_to_image/train_text_to_image_lora.py
@@ -693,7 +693,7 @@ def main():
         else:
             accelerator.print(f"Resuming from checkpoint {path}")
             #accelerator.load_state(os.path.join(args.output_dir, path))
-            accelerator.load_state(os.path.join('CVH-vn1210/hair-model', path))
+            accelerator.load_state(os.path.join('./hair-model', path))
             global_step = int(path.split("-")[1])
 
             resume_global_step = global_step * args.gradient_accumulation_steps
